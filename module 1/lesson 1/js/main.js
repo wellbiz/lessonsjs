dfb1b6d5c764e1bccbecef12aadd899b22f2e3d3
Dmitry Taras <wellbiz@ya.ru>
2021-11-19 20:56:54 +0300

    lesson01 modificated

--- module 1/lesson 1/js/main.js
+++ module 1/lesson 1/js/main.js
@@ -1,14 +1,30 @@
 "use strict";
-/*first commit*/
-const nameGoods = "Охота Крепкое, 9%, 0.5";
-const countGoods = "5 банок";
-const typeGoods = "Напитки слабоалкогольные, пенные, нефильтрованные";
-const priceGoods = "52.99 руб.";
+{
+  const nameGoods = "Охота Крепкое, 9%, 0.5";
+  const countGoods = "5 банок";
+  const typeGoods = "Напитки слабоалкогольные, пенные, нефильтрованные";
+  const priceGoods = "52.99 руб.";
 
-console.log(`
+  console.log(`
 Наименование товара: ${nameGoods};\n
 Количество товара: ${countGoods};\n
 Категория товара: ${typeGoods};\n
 Цена товара: ${priceGoods};\n
 Общая стоимость товара: ${parseInt(countGoods) * parseFloat(priceGoods)} руб.
 `);
+}
+
+{
+  const nameGoods = "доширак со вкусом вяленого моржа";
+  const countGoods = 3;
+  const typeGoods = "быстрорастворимая еда";
+  const priceGoods = 15.92;
+
+  console.log(`
+    Наименование товара: ${nameGoods};
+    Количество товара: ${countGoods};
+    Категория товара: ${typeGoods};
+    Цена товара: ${priceGoods};
+    Общая стоимость товара: ${countGoods * priceGoods} руб.
+    `);
+}
