aa5903ee505bcfc9e8fd2a15fa298c5e90a92ce6
Parit <wellbiz@ya.ru>
2021-12-01 20:24:16 +0300

    Delete lesson 7 directory

--- lesson 7/task02.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const allCashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
-
-const getAverageValue = (arr) =>
-  arr.reduce((acc, item) => acc + item, 0) / arr.length;
-
-console.log(~~getAverageValue(allCashbox));
