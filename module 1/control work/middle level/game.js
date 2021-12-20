c83867c88ea4a88ce60abbe4f9cdb9c071f4e2ed
Parit <wellbiz@ya.ru>
2021-12-20 10:02:25 +0300

    fix: don't worked exit from the game

--- module 1/control work/middle level/game.js
+++ module 1/control work/middle level/game.js
@@ -1,5 +1,5 @@
 /* eslint-disable prefer-const */
-'use strict';
+"use strict";
 /* eslint-disable require-jsdoc */
 (() => {
   let ballsPC = 5;
@@ -14,20 +14,16 @@
   };
 
   const getUserNumber = (ballsUser) => {
-    const numberByUser = prompt(
-        `Введите кол-во шариков от 1 до ${ballsUser}`,
-    );
+    const numberByUser = prompt(`Введите кол-во шариков от 1 до ${ballsUser}`);
     if (numberByUser === null) {
       return false;
     } else {
-      if (isNaN(numberByUser) || numberByUser === '') {
-        alert('Вы не ввели число, введите ещё раз!');
+      if (isNaN(numberByUser) || numberByUser === "") {
+        alert("Вы не ввели число, введите ещё раз!");
         return getUserNumber(ballsUser);
       }
       if (Number(numberByUser) > ballsUser) {
-        alert(
-            'Вы ввели больше шариков, чем у вас есть. Попробуйте еще раз',
-        );
+        alert("Вы ввели больше шариков, чем у вас есть. Попробуйте еще раз");
         return getUserNumber(ballsUser);
       }
       return Number(numberByUser);
@@ -35,42 +31,27 @@
   };
 
   const game = () => {
-    const gameOver = () => {
-      if (ballsPC < 1 || ballsUser < 1) {
-        const isWinUser = ballsPC < 1 ? 'выиграли' : 'проиграли';
 
-        alert(`Вы ${isWinUser}`);
-        ballsPC = 5;
-        ballsUser = 5;
-        step = 1;
-        const toGame = confirm('Хотите сыграть еще раз?');
-        if (toGame) {
-          return game();
-        } else {
-          return;
-        }
-      }
-      console.log(`кол-во шариков у юзера: ${ballsUser}`);
-      console.log(`кол-во шариков у бота: ${ballsPC}`);
-    };
 
     // бот угадывает у юзера
     if (step) {
       console.log(`Ход юзера.`);
-      const reminderOfDivide = genRnd(0, 1);
       console.log(`бот попытался отгадать`);
+
+      const reminderOfDivide = genRnd(0, 1);
       const numUser = getUserNumber(ballsUser);
+      
       if (numUser === false) return;
       const isBotGuessed =
-                (!(numUser % 2) && !reminderOfDivide) ||
-                (numUser % 2 && reminderOfDivide);
+        (!(numUser % 2) && !reminderOfDivide) ||
+        (numUser % 2 && reminderOfDivide);
 
       // eslint-disable-next-line max-len
       console.log(
-          // eslint-disable-next-line max-len
-          `Юзер загадал ${numUser}, бот - ${
-                    isBotGuessed ? 'угадал' : 'не угадал'
-          }`,
+        // eslint-disable-next-line max-len
+        `Юзер загадал ${numUser}, бот - ${
+          isBotGuessed ? "" : "не "
+        }угадал`
       );
 
       if (isBotGuessed) {
@@ -82,16 +63,30 @@
       }
 
       --step;
-      gameOver();
+      if (ballsPC < 1 || ballsUser < 1) {
+
+        alert(`Вы ${ballsPC < 1 ? "вы" : "про"}играли`);
+        ballsPC = 5;
+        ballsUser = 5;
+        step = 1;
+        const toGame = confirm("Хотите сыграть еще раз?");
+        if (toGame) {
+          return game();
+        } else {
+          return ;
+        }
+      }
+      console.log(`кол-во шариков у юзера: ${ballsUser}`);
+      console.log(`кол-во шариков у бота: ${ballsPC}`);
     } else {
       const numbot = genRnd(1, ballsPC);
       console.log(`Ход бота. Бот загадал...`);
-      const choise = confirm('Число нечётное?');
+      const choise = confirm("Число нечётное?");
       const isUserGuessed =
-                (!(numbot % 2) && !choise) || (numbot % 2 && choise);
+        (!(numbot % 2) && !choise) || (numbot % 2 && choise);
 
       // eslint-disable-next-line max-len
-      console.log(`Юзер ${isUserGuessed ? '' : 'не'}угадал`);
+      console.log(`Юзер ${isUserGuessed ? "" : "не"}угадал`);
 
       if (isUserGuessed) {
         ballsPC -= numbot;
@@ -101,7 +96,20 @@
         ballsUser -= numbot;
       }
       ++step;
-      gameOver();
+      if (ballsPC < 1 || ballsUser < 1) {
+        alert(`Вы ${ballsPC < 1 ? "вы" : "про"}играли`);
+        ballsPC = 5;
+        ballsUser = 5;
+        step = 1;
+        const toGame = confirm("Хотите сыграть еще раз?");
+        if (toGame) {
+          return game();
+        } else {
+          return ;
+        }
+      }
+      console.log(`кол-во шариков у юзера: ${ballsUser}`);
+      console.log(`кол-во шариков у бота: ${ballsPC}`);
     }
     game();
   };
